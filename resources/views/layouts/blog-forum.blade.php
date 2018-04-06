5dc40393cd971c423b0a9f63fe5e398a9d37abab
septke <seppecasier@hotmail.com>
2018-04-06 14:30:36 +0200

    Laravel_Forum

--- resources/views/layouts/blog-forum.blade.php
+++ resources/views/layouts/blog-forum.blade.php
@@ -37,23 +37,8 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </div>
-            <a class="navbar-brand" href="#">Reddit 2.0</a>
+            <a class="navbar-brand" href="/threads">Reddit 2.0</a>
         </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
-        </div>
-        <!-- /.navbar-collapse -->
     </div>
     <!-- /.container -->
 </nav>
